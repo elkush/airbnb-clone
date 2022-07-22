e10548cae08fcfe05648e1e88b4b2bbb6adb79b0
Elena Kushner <85772107+elkush@users.noreply.github.com>
2022-07-22 15:20:01 +1000

    static in react

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="nav-bar">
+            <img alt="" className="airbnb-icon" src="./images/airbnb1.png"></img>
+        </nav>
+    )
+}
